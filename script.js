5f415cf46aa41a80a2164a533505cfd8bf35c1f0
musango willy <musangowilly@gmail.com>
2023-12-04 11:50:00 +0300

    updating

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//personal details
+const personalDetails{
+
+    fullName:"musango Willy";
+    age=25;
+    gender:"male";
+    email:"musangowilly@gmail.com";
+    hobbies:"watching football,music",
+}
+//
+const educaction{
+
+}
